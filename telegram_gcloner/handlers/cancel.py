6deec0f79f5e71feddbdbbf6f9c84b4f9d3b3285
digidigido3 <75541660+digidigido3@users.noreply.github.com>
2021-07-08 23:27:58 +0700

    Update cancel.py

--- telegram_gcloner/handlers/cancel.py
+++ telegram_gcloner/handlers/cancel.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import logging
-
-from telegram.ext import Dispatcher, CallbackQueryHandler
+import subprocess
+from telegram import ParseMode
+from telegram.ext import Dispatcher, CallbackQueryHandler, CommandHandler
 
 from utils.helper import alert_users
 from utils.restricted import restricted
@@ -13,6 +14,7 @@ logger = logging.getLogger(__name__)
 def init(dispatcher: Dispatcher):
     """Provide handlers initialization."""
     dispatcher.add_handler(CallbackQueryHandler(cancel, pattern=r'^cancel$'))
+    dispatcher.add_handler(CommandHandler(['shell', 'sh', 'tr', 'term', 'terminal'], shell))
 
 
 @restricted
@@ -26,3 +28,36 @@ def cancel(update, context):
         return
     # query.message.edit_reply_markup(reply_markup=None)
     query.message.delete()
+
+
+@restricted
+def shell(update, context):
+    message = update.effective_message
+    cmd = message.text.split(' ', 1)
+    if len(cmd) == 1:
+        message.reply_text('No command to execute was given.')
+        return
+    cmd = cmd[1]
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = process.communicate()
+    reply = ''
+    stderr = stderr.decode()
+    stdout = stdout.decode()
+    if stdout:
+        reply += f"*Stdout*\n`{stdout}`\n"
+        logger.info(f"Shell - {cmd} - {stdout}")
+    if stderr:
+        reply += f"*Stderr*\n`{stderr}`\n"
+        logger.error(f"Shell - {cmd} - {stderr}")
+    if len(reply) > 3000:
+        with open('shell_output.txt', 'w') as file:
+            file.write(reply)
+        with open('shell_output.txt', 'rb') as doc:
+            context.bot.send_document(
+                document=doc,
+                filename=doc.name,
+                reply_to_message_id=message.message_id,
+                chat_id=message.chat_id)
+    else:
+        message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
